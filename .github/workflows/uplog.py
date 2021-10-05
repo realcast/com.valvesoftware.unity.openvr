27f94df541cffc1ed4ac005a0e86428f1a606069
diego fernandez-bravo <diego@realcast.io>
2021-10-05 12:10:26 +0200

    Fix GitHub workflow

--- .github/workflows/uplog.py
+++ .github/workflows/uplog.py
@@ -22,7 +22,6 @@ if 'VER' in os.environ:
 #    repository = os.environ['repository']
     date = dt.datetime.now()
     Date = date.strftime("%d-%B-%Y")
-    print("tata")
     with open('package/CHANGELOG.md', 'r') as read_obj, open('package/dummy_file.md', 'w') as write_obj:
         for line in read_obj:
             if '[Semantic Versioning]' in line :
@@ -35,11 +34,8 @@ if 'VER' in os.environ:
                 write_obj.write(commit)
             else :
                 write_obj.write(line)
-    print("toto")
     os.remove('package/CHANGELOG.md')
-    print("titi")
     os.rename('package/dummy_file.md', 'package/CHANGELOG.md')
-    print("tutu")
 
 # This script updates the file CHANGELOG.md by adding all the commits made on
 # the branch use to start this workflow
